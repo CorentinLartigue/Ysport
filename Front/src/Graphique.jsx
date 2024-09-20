2483a43652914a4cc1da3e409c902ddd2b91106f
Corentin Lartigue <corentin.lartigue.cl@gmail.com>
2024-09-20 09:16:55 +0200

    Dernier Commit

--- Front/src/Graphique.jsx
+++ Front/src/Graphique.jsx
@@ -1,8 +1,8 @@
 import './Graphique.css'
 import ClassementInfra from './assets/classement_infra_par_dept.webp';
-import ClassementTypeInfra from './assets/classement_type_infra_N-A.webp';
-import RepartitionGironde from './assets/repartition_type_de_zone_en_gironde.webp';
-import RepartitionNouvelleAquitaine from './assets/repartition_type_de_zone_en_Nouvelle-Aquitaine.webp';
+import RepartitionAccessHandicape from './assets/repartition_accees_handicapees.webp';
+import AccessHandicapGironde from './assets/infra_avec_le_moins_daccees_handicapees_en_gironde.webp';
+import AccessHandicapNouvelleAquitaine from './assets/infra_avec_le_moins_daccees_handicapees_en_N-A.webp';
 import Top10InfraGironde from './assets/top_10_infra_en_gironde.webp';
 import Top10InfraNouvelleAquitaine from './assets/top_10_infra_en_Nouvelle-Aquitaine.webp';
 
@@ -13,11 +13,11 @@ function Graphique(){
        <div class="app-container">
             <div class="content">
                 <img src={ClassementInfra} className="graphic-item" alt="Classement Infra"></img>
-                <img src={ClassementTypeInfra} className="graphic-item"  alt="Classement Type Infra"></img>
-                <img src={RepartitionGironde} className="graphic-item" alt="Repartition Gironde"></img>
-                <img src={RepartitionNouvelleAquitaine} className="graphic-item" alt="Repartition Nouvelle Aquitaine"></img>
-                <img src={Top10InfraGironde} className="graphic-item" alt="Top 10 Infra Gironde"></img>
                 <img src={Top10InfraNouvelleAquitaine} className="graphic-item" alt="Top 10 Infra Nouvelle Aquitaine"></img>
+                <img src={Top10InfraGironde} className="graphic-item" alt="Top 10 Infra Gironde"></img>
+                <img src={RepartitionAccessHandicape} className="graphic-item"  alt="RepartitionAccessHandicape"></img>
+                <img src={AccessHandicapNouvelleAquitaine} className="graphic-item" alt="AccessHandicapNouvelleAquitaine"></img>
+                <img src={AccessHandicapGironde} className="graphic-item" alt="AccessHandicapGironde"></img>
             </div>
         </div>
     </>
